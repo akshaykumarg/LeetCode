8df04c686adc956b2233352a29ad571d680c8725
Akshay Kumar G <45440749+iamakshaykumarg@users.noreply.github.com>
2019-01-04 02:55:27 -0500

    92 problem is modified

--- LinkedLists/92. Reverse Linked List II.java
+++ LinkedLists/92. Reverse Linked List II.java
@@ -1,114 +1,29 @@
+
 class Solution {
-    //Using BFS to solve the problem.
-    HashSet<String> set;
-    Queue<String> q;
-    public int slidingPuzzle(int[][] board) {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
         
-        q = new LinkedList<>();
-        set = new HashSet<>();
-        String result = "123450";
-        StringBuilder s = new StringBuilder();
-        for(int i=0;i<2;i++)
-            for(int j=0;j<3;j++)
-                s.append((char)(board[i][j]+'0'));
-        // System.out.println(s);
-        q.offer(s.toString());
-        set.add(s.toString());
-        int count=0;
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                String temp = q.poll();
-                if(temp.equals(result))
-                    return count;
-                trace(temp);
-            }
-            count++;
+        ListNode prev1 = new ListNode(0);
+        prev1.next = head;
+        ListNode prev = prev1;
+        ListNode forward = head;
+        
+        for(int i=1;i<m;i++){
+            prev = prev.next;
+            forward = forward.next;
         }
         
-        return -1;
+        ListNode temp = forward.next;
+        
+        for(int i=m;i<n;i++){
+            
+            ListNode x = temp.next;
+            temp.next = prev.next;
+            prev.next = temp;    
+            forward.next = x;
+            temp = x;
+            
+        }
+        
+        return prev1.next;
     }
-    void trace(String s){
-        int i = s.indexOf('0');
-        
-        if(i==0 || i==3){
-            char[] c = s.toCharArray();
-            char temp = c[i+1];
-            c[i+1]=c[i];
-            c[i]=temp;
-            String x = new String(c);
-            if(!set.contains(x)){
-                q.offer(x);
-                set.add(x);
-            }
-                
-            
-            int tmp = (i+3)%6;
-            c = s.toCharArray();
-            temp = c[tmp];
-            c[tmp]=c[i];
-            c[i]=temp;
-            x = new String(c);
-            if(!set.contains(x)){
-                q.offer(x);
-                set.add(x);
-            }
-        }
-        else if(i==2 || i==5){
-            char[] c = s.toCharArray();
-            char temp = c[i-1];
-            c[i-1]=c[i];
-            c[i]=temp;
-            String x = new String(c);
-            if(!set.contains(x)){
-                q.offer(x);
-                set.add(x);
-            }
-            
-            int tmp = (i+3)%6;
-            c = s.toCharArray();
-            temp = c[tmp];
-            c[tmp]=c[i];
-            c[i]=temp;
-            x = new String(c);
-            if(!set.contains(x)){
-                q.offer(x);
-                set.add(x);
-            }
-        }
-        else{
-            char[] c = s.toCharArray();
-            char temp = c[i+1];
-            c[i+1]=c[i];
-            c[i]=temp;
-            String x = new String(c);
-            if(!set.contains(x)){
-                q.offer(x);
-                set.add(x);
-            }
-            
-            c = s.toCharArray();
-            temp = c[i-1];
-            c[i-1]=c[i];
-            c[i]=temp;
-            x = new String(c);
-            if(!set.contains(x)){
-                q.offer(x);
-                set.add(x);
-            }
-            
-            int tmp = (i+3)%6;
-            c = s.toCharArray();
-            temp = c[tmp];
-            c[tmp]=c[i];
-            c[i]=temp;
-            x = new String(c);
-            if(!set.contains(x)){
-                q.offer(x);
-                set.add(x);
-            }
-            
-        }
-    }
-    
-}
+}   
