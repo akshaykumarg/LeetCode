8c6d679e548edc2cda26b2bbfac453784c44a927
Akshay Kumar G <45440749+iamakshaykumarg@users.noreply.github.com>
2019-01-04 14:36:45 -0500

    Create 304. Range Sum Query 2D - Immutable.java

---
+++ 304. Range Sum Query 2D - Immutable.java
@@ -0,0 +1,21 @@
+class NumMatrix {
+
+    int[][] dp;
+    
+    public NumMatrix(int[][] matrix) {
+        if(matrix.length==0) return;
+        dp = new int[matrix.length][matrix[0].length+1];
+        for(int i=0;i<matrix.length;i++)
+            for(int j=0;j<matrix[0].length;j++)
+                dp[i][j+1]=dp[i][j]+matrix[i][j];
+        
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum=0;
+        for(int i=row1;i<=row2;i++){
+            sum+=(dp[i][col2+1]-dp[i][col1]);
+        }
+        return sum;
+    }
+}
