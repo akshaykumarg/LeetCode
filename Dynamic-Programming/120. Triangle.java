146058992b82bdacc83d8c651efd9cc7b4969409
Akshay Kumar G <45440749+akshaykumarg@users.noreply.github.com>
2019-01-26 15:31:21 -0500

    Update 120. Triangle.java

--- Dynamic-Programming/120. Triangle.java
+++ Dynamic-Programming/120. Triangle.java
@@ -1,14 +1,18 @@
-// Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+/*
 
-// For example, given the following triangle
+Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
 
-// [
-//      [2],
-//     [3,4],
-//    [6,5,7],
-//   [4,1,8,3]
-// ]
-// The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+For example, given the following triangle
+
+[
+     [2],
+    [3,4],
+   [6,5,7],
+  [4,1,8,3]
+]
+The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+
+*/
  
 class Solution {
     public int minimumTotal(List<List<Integer>> arr) {
