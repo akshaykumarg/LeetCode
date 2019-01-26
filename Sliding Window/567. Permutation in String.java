39912c801f2c344239d49fc23bbf91270247a2d9
Akshay Kumar G <45440749+akshaykumarg@users.noreply.github.com>
2019-01-26 15:11:23 -0500

    Update 567. Permutation in String.java

--- Sliding Window/567. Permutation in String.java
+++ Sliding Window/567. Permutation in String.java
@@ -1,7 +1,18 @@
-// Given a string s, you are allowed to convert it to a palindrome by 
-//adding characters in front of it. Find and return the shortest palindrome 
-//you can find by performing this transformation.
-    
+/*
+
+Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. In other words, one of the first string's permutations is the substring of the second string.
+
+Example 1:
+Input:s1 = "ab" s2 = "eidbaooo"
+Output:True
+
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+Input:s1= "ab" s2 = "eidboaoo"
+Output: False
+
+*/
+
 class Solution {
     public String shortestPalindrome(String s) {
        	int n = s.length();
